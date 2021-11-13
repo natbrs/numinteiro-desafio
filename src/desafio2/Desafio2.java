5359ddff16ae817c70541fb673d72fa4ca7e7a53
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2021-11-13 19:43:53 -0300

    Desafio Números Inteiros - JAVA

---
+++ src/desafio2/Desafio2.java
@@ -0,0 +1,16 @@
+package desafio2;
+
+import javax.swing.JOptionPane;
+
+public class Desafio2 {
+
+    public static void main(String[] args) {
+
+         int i;
+        
+         for(i = 200; i >= 100; i--){
+         System.out.println(i);
+        
+        }   
+    }
+}
